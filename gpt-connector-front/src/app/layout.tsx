84129d0069f3954b32c8c839a5d93866d19a6c02
ikhyeons <skantrkwl789@naver.com>
2024-01-13 22:51:01 +0900

    Update : 프로젝트 초기 세팅

---
+++ gpt-connector-front/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "gptConnector Log viewer",
+  description: "gptConnector Log viewer",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
