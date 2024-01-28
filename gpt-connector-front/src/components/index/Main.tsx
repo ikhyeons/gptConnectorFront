da6b9f152aa7400ce2d250769abaef300607d54c
ikhyeons <skantrkwl789@naver.com>
2024-01-28 23:03:22 +0900

    Update : 쿠키 있을 시 리디렉션

--- gpt-connector-front/src/components/index/Main.tsx
+++ gpt-connector-front/src/components/index/Main.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import style from "./main.module.scss";
 import ActionDiv from "./actionDiv";
-
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/navigation";
 function Main() {
+  const [cookies, setcookie, deleteCookie] = useCookies(["token"]);
+  const router = useRouter();
+  useEffect(() => {
+    if (cookies.token) router.push("/viewer");
+  }, []);
   return (
     <main className={style.main}>
       <ActionDiv />
